356ad43f4101ef0431e8cf2ed6ce643abaf62c5d
Aleko Georgiev <61247544+unamusedchemical@users.noreply.github.com>
2023-07-06 23:16:53 +0300

    Initial Commit

---
+++ StamSoftProizvodstvena/StamSoftProizvodstvenaApp.swift
@@ -0,0 +1,17 @@
+//
+//  StamSoftProizvodstvenaApp.swift
+//  StamSoftProizvodstvena
+//
+//  Created by Aleko Georgiev on 6.07.23.
+//
+
+import SwiftUI
+
+@main
+struct StamSoftProizvodstvenaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
